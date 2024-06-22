af850106bb3f340cde0baab418de0af370c196e8
namkuner <namkuner@gmail.com>
2024-06-22 20:12:24 +0700

    report bang diem

--- ThiTN/SQL/Nam.sql
+++ ThiTN/SQL/Nam.sql
@@ -1,6 +1,6 @@
 CREATE VIEW [dbo].[V_DS_PHANMANH]
 AS
-SELECT  TENCN=PUBS.name, TENSERVER= subscriber_server
+SELECT  TENCN=PUBS.description, TENSERVER= subscriber_server
    FROM dbo.sysmergepublications PUBS,  dbo.sysmergesubscriptions SUBS
    WHERE PUBS.pubid= SUBS.PUBID  AND PUBS.publisher <> SUBS.subscriber_server
 
